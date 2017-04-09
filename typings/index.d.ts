c9a373f075f43c325db60d610753e4bd17034b7c
Pavel Birukov <birukov.pavel@gmail.com>
2017-04-09 12:55:48 +0300

    Autoinstall typings and remove them from Git. Update ts. Add includes section in tsconfig

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/assert/index.d.ts" />
-/// <reference path="globals/global/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/underscore/index.d.ts" />
