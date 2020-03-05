d8e565ea21b88349d6c72bee0593cfda35b97f67
andy <andy@googlemail.com>
2020-03-05 17:54:01 +0000

    Upgrade to Typescript 3.8 and latest versions of packages

--- test/either.ts
+++ test/either.ts
@@ -1,114 +1,114 @@
-import {Either, either} from '../src/either'
+import { Either, either } from '../src/either'
 
 import * as assert from 'assert'
 
 describe('Either', () => {
 
-    it('Case of', () => {
+  it('Case of', () => {
 
-        assert.ok(Either.left<string, number>('on noes')
-            .caseOf({
-                left: s => true,
-                right: n => false
-            }));
+    assert.ok(Either.left<string, number>('on noes')
+      .caseOf({
+        left: s => true,
+        right: n => false
+      }));
 
-        assert.ok(Either.right<string, number>(1)
-            .caseOf({
-                left: s => false,
-                right: n => true
-            }));
-    });
+    assert.ok(Either.right<string, number>(1)
+      .caseOf({
+        left: s => false,
+        right: n => true
+      }));
+  });
 
-    it('isLeft', () => {
-        assert.ok(Either.left(2).isLeft());
+  it('isLeft', () => {
+    assert.ok(Either.left(2).isLeft());
 
-        assert.strictEqual(Either.right(2).isLeft(), false);
-    });
+    assert.strictEqual(Either.right(2).isLeft(), false);
+  });
 
-    it('isRight', () => {
-        assert.ok(Either.right(2).isRight());
+  it('isRight', () => {
+    assert.ok(Either.right(2).isRight());
 
-        assert.strictEqual(Either.left(2).isRight(), false);
-    });
+    assert.strictEqual(Either.left(2).isRight(), false);
+  });
 
-    it('Do', () => {
+  it('Do', () => {
 
-        assert.throws(() =>
-            either('l', null).do({
-                left: (l) => { throw 'left'; },
-                right: (r) => { throw 'right'; },
-            }),
-            /left/,
-            'do has a `left` path'
-        );
+    assert.throws(() =>
+      either('l', null).do({
+        left: (l) => { throw 'left'; },
+        right: (r) => { throw 'right'; },
+      }),
+      /left/,
+      'do has a `left` path'
+    );
 
-        assert.throws(() =>
-            either(null, 'r').do({
-            left: (l) => { throw 'left'; },
-            right: (r) => { throw 'right'; },
-            }),
-            /right/,
-            'do has a `right` path'
-        );
+    assert.throws(() =>
+      either(null, 'r').do({
+        left: (l) => { throw 'left'; },
+        right: (r) => { throw 'right'; },
+      }),
+      /right/,
+      'do has a `right` path'
+    );
 
-    });
+  });
 
-    it('Bind', () => {
+  it('Bind', () => {
 
-        assert.ok(Either.right<string, number>(2)
-            .bind(n => Either.right<string, number>(n * 2))
-            .bind(n => Either.right<string, number>(n * 2))
-            .caseOf({
-                left: s => false,
-                right: n => n === 8
-            }));
+    assert.ok(Either.right<string, number>(2)
+      .bind(n => Either.right<string, number>(n * 2))
+      .bind(n => Either.right<string, number>(n * 2))
+      .caseOf({
+        left: s => false,
+        right: n => n === 8
+      }));
 
-        assert.ok(Either.right<string, number>(2)
-            .bind(n => Either.right<string, number>(n * 2))
-            .bind(n => Either.left<string, number>('nooo'))
-            .caseOf({
-                left: s => s === 'nooo',
-                right: n => false
-            }));
-    });
+    assert.ok(Either.right<string, number>(2)
+      .bind(n => Either.right<string, number>(n * 2))
+      .bind(n => Either.left<string, number>('nooo'))
+      .caseOf({
+        left: s => s === 'nooo',
+        right: n => false
+      }));
+  });
 
-    it('Lift', () => {
+  it('Lift', () => {
 
-        assert.ok(Either.right<string, number>(2)
-            .lift(n => n * 2)
-            .lift(n => n * 2)
-            .caseOf({
-                left: s => false,
-                right: n => n === 8
-            }));
+    assert.ok(Either.right<string, number>(2)
+      .lift(n => n * 2)
+      .lift(n => n * 2)
+      .caseOf({
+        left: s => false,
+        right: n => n === 8
+      }));
 
-        assert.ok(Either.right<string, number>(2)
-            .lift(n => n * 2)
-            .lift(n => <number>null)
-            .caseOf({
-                left: s => false,
-                right: n => !n
-                // unlike Maybe, lifting a null into Either has no special behaviour
-                // so try to avoid this kind of sociopathic behaviour
-            }));
-    });
+    assert.ok(Either.right<string, number>(2)
+      .lift(n => n * 2)
+      .lift(n => null)
+      .caseOf({
+        left: s => false,
+        right: n => !n
+        // unlike Maybe, lifting a null into Either has no special behaviour
+        // so try to avoid this kind of sociopathic behaviour
+      }));
+  });
 
-    it('Constructors', () => {
+  it('Constructors', () => {
 
-        assert.ok(either<string, number>('oh noes')
-            .caseOf({
-                left: s => s === 'oh noes',
-                right: n => false
-            }));
+    assert.ok(either<string, number>('oh noes')
+      .caseOf({
+        left: s => s === 'oh noes',
+        right: n => false
+      }));
 
-        assert.ok(either<string, number>(null, 123)
-            .caseOf({
-                left: s => false,
-                right: n => n === 123
-            }));
+    assert.ok(either<string, number>(undefined, 123)
+      .caseOf({
+        left: s => false,
+        right: n => n === 123
+      }));
 
-        assert.throws(() => either('not both', 123), /both/);
-        assert.throws(() => either<string,number>(), /neither/);
-    });
+    assert.throws(() => either('not both', 123), /both/);
+    assert.throws(() => either<string, number>(), /neither/);
+  });
 
 })
